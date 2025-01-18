334f850c1163fe4111f5f3d445c79219abe5edcd
23N3038 <megumi.sano.7h@stu.hosei.ac.jp>
2025-01-18 23:02:27 +0900

    nyan

--- functions.js
+++ functions.js
@@ -3,35 +3,31 @@ gsap.registerPlugin(ScrollTrigger);
 const imageContainer = document.querySelector('.image-container');
 const layers = document.querySelectorAll('.image > *');
 
-// 初期状態を設定
 gsap.set(layers, { opacity: 0 });
-gsap.set('#layer01', { opacity: 1 });
+gsap.set('#midori', { opacity: 1 });  // midori is always visible
 
 ScrollTrigger.create({
-    trigger: imageContainer,
-    start: 'top top',
-    end: '+=3000', // スクロール全体の長さを調整
-    pin: true,
-    scrub: true,
-
-    onUpdate: (self) => {
-        if (self.progress < 0.2) {
-            // #layer01を表示、それ以外は非表示
-            gsap.to('#layer01', { opacity: 1, duration: 1 });
-            gsap.to('#layer02, #layer03, #layer04', { opacity: 0, duration: 1 });
-        } else if (self.progress >= 0.2 && self.progress < 0.5) {
-            // #layer02を長い間表示（#layer01はそのまま）
-            gsap.to('#layer01', { opacity: 1, duration: 1 });
-            gsap.to('#layer02', { opacity: 1, duration: 1 });
-            gsap.to('#layer03, #layer04', { opacity: 0, duration: 1 });
-        } else if (self.progress >= 0.5 && self.progress < 0.75) {
-            // #layer03をゆっくり表示（#layer01, #layer02はそのまま）
-            gsap.to('#layer01, #layer02', { opacity: 1, duration: 1 });
-            gsap.to('#layer03', { opacity: 1, duration: 4 }); // ゆっくり表示
-            gsap.to('#layer04', { opacity: 0, duration: 1 });
-        } else {
-            // すべてのレイヤーを表示
-            gsap.to('#layer01, #layer02, #layer03, #layer04', { opacity: 1, duration: 1 });
-        }
-    },
+  trigger: imageContainer,
+  start: 'top top',
+  end: '+=2000',
+  pin: true,
+  scrub: true,
+  // markers: true, 
+  onUpdate: (self) => {
+    if (self.progress < 0.25) {
+      gsap.to('#papa1', { opacity: 1 });
+      gsap.to('#ball1, #ball2, #ball3, #papa2, #!, #!!', { opacity: 0 });
+    } else if (self.progress >= 0.25 && self.progress < 0.5) { 
+      gsap.to('#papa1, #ball1', { opacity: 1 }); 
+      gsap.to('#ball2, #ball3, #papa2, #!, #!!', { opacity: 0 }); 
+    } else if (self.progress >= 0.5 && self.progress < 0.75) {
+      gsap.to('#papa1, #ball1, #ball2', { opacity: 1 }); 
+      gsap.to('#ball3, #papa2, #!, #!!', { opacity: 0 }); 
+    } else if (self.progress >= 0.75 && self.progress < 1) {
+      gsap.to('#papa1, #ball1, #ball2, #ball3', { opacity: 1 });
+      gsap.to('#papa2, #!, #!!', { opacity: 0 });
+    } else {
+      gsap.to('#papa1, #ball1, #ball2, #ball3, #papa2, #!, #!!', { opacity: 1 });
+    }
+  },
 });
